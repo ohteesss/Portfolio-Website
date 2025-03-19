38e99f043a4022733e280f6f4853350a9f2b9858
Ojo <oluwatobiojo2911@gmail.com>
2025-03-19 05:29:23 +0100

    Add title to TechnologyIcon and duration to ExperienceType interfaces

--- src/app.interface.ts
+++ src/app.interface.ts
@@ -3,11 +3,13 @@ import { ReactNode } from "react";
 export interface TechnologyIcon {
   src: string;
   alt: string;
+  title: string;
 }
 
 export interface ExperienceType {
   role: string;
   companyName: string;
+  duration: string;
   description: string;
 }
 
