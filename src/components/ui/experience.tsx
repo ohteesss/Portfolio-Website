d5fe88c99bf54a40ccffa6411f7f5b29475aa5e3
Ojo <oluwatobiojo2911@gmail.com>
2025-03-19 05:30:10 +0100

    Add duration prop to ExperienceComponent and update text rendering

--- src/components/ui/experience.tsx
+++ src/components/ui/experience.tsx
@@ -8,6 +8,7 @@ interface Experience extends ExperienceType {
 function ExperienceComponent({
   role,
   companyName,
+  duration,
   description,
   index,
 }: Experience) {
@@ -25,14 +26,7 @@ function ExperienceComponent({
         <h6>{role || "Role"}</h6>
         <h3>{companyName || "Company Name"}</h3>
         <SplitText
-          text={
-            description ||
-            `description: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Debitis praesentium natus possimus expedita voluptatum adipisci
-          similique sed ipsa consequatur, excepturi asperiores vitae neque.
-          Laborum quod optio illo nulla magnam? Neque est, inventore ad dolorum
-          omnis veniam fugit consectetur vero itaque!`
-          }
+          text={duration}
           delay={10}
           transforms={["translate3d(0,0,0)"]}
         />
@@ -47,11 +41,7 @@ function ExperienceComponent({
         <br />
         <br />
         <SplitText
-          text={`description: Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Debitis praesentium natus possimus expedita voluptatum adipisci
-          similique sed ipsa consequatur, excepturi asperiores vitae neque.
-          Laborum quod optio illo nulla magnam? Neque est, inventore ad dolorum
-          omnis veniam fugit consectetur vero itaque!`}
+          text={description}
           delay={10}
           transforms={["translate3d(0,0,0)"]}
         />
